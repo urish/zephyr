08d1f41b0ac3ee0db6f838d6f80bee51e45a0069
Benny Meisels <benny.meisels@gmail.com>
2019-04-12 09:27:38 +0300

    Added il0373, not complete

---
+++ drivers/display/display_il0373.h
@@ -0,0 +1,32 @@
+#ifndef __IL0373_DRV_H__
+#define __IL0373_DRV_H__
+
+
+#define IL0373_CMD_PANEL_SETTING         0x00
+#define IL0373_CMD_POWER_SETTING         0x01
+#define IL0373_CMD_POWER_OFF             0x02
+#define IL0373_CMD_POWER_OFF_SEQUENCE    0x03
+#define IL0373_CMD_POWER_ON              0x04
+#define IL0373_CMD_POWER_ON_MEASURE      0x05
+#define IL0373_CMD_BOOSTER_SOFT_START    0x06
+#define IL0373_CMD_DEEP_SLEEP            0x07
+#define IL0373_CMD_DTM1                  0x10
+#define IL0373_CMD_DATA_STOP             0x11
+#define IL0373_CMD_DISPLAY_REFRESH       0x12
+#define IL0373_CMD_DTM2                  0x13
+#define IL0373_CMD_PDTM1                 0x14
+#define IL0373_CMD_PDTM2                 0x15
+#define IL0373_CMD_PDRF                  0x16
+#define IL0373_CMD_LUT1                  0x20
+#define IL0373_CMD_LUTWW                 0x21
+#define IL0373_CMD_LUTBW                 0x22
+#define IL0373_CMD_LUTWB                 0x23
+#define IL0373_CMD_LUTBB                 0x24
+#define IL0373_CMD_PLL                   0x30
+#define IL0373_CMD_CDI                   0x50
+#define IL0373_CMD_RESOLUTION            0x61
+#define IL0373_CMD_VCM_DC_SETTING        0x82
+#define IL0373_CMD_PARTIAL_WINDOW        0x90
+
+
+#endif
